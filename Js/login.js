bf77f7c6720fc6ebed9c3dad9958ebc6b20f0fb1
Muhammad Danish <55913331+Danishkhilji@users.noreply.github.com>
2021-09-20 02:03:02 -0700

    I have added ligon.js file
    I have added ligon.js file today, yesterday when i am trying to run my app but an error was accord, now I check there is problem with uploading because all file was uploaded expect login.js

---
+++ Js/login.js
@@ -0,0 +1,29 @@
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+const googleLogin=()=>{
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+        var user = result.user;
+    setTimeout(() => {
+        location.replace("./html/game.html")    
+    }, 2000);
+      
+    }).catch((error) => {
+    });
+
+}
+
+const logIn =()=>{
+    googleLogin();
+}
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+     
+    }
+})
